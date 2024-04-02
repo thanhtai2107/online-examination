c0087b89423f4a3be6ac54426a77a0c77d491f37
thanhtai2107 <thanhtai21701@gmail.com>
2024-04-02 16:22:15 +0700

    feat: add add-teacher and update-teacher form

--- Frontend/online-examination/src/pages/ListTeacher.js
+++ Frontend/online-examination/src/pages/ListTeacher.js
@@ -20,15 +20,25 @@ function ListTeacher() {
       date_create: "20-03-2024",
     },
   ];
-  const handlePopupAddTeachForm = () => {};
-  const handlePopupUpdateTeacherForm = () => {};
+  const handlePopupAddTeachForm = () => {
+    document.getElementById("add-teacher").style.display = "flex";
+  };
+  const handleCloseAddTeacherForm = () => {
+    document.getElementById("add-teacher").style.display = "none";
+  };
+  const handlePopupUpdateTeacherForm = () => {
+    document.getElementById("update-teacher").style.display = "flex";
+  };
+  const handleCloseUpdateTeacherForm = () => {
+    document.getElementById("update-teacher").style.display = "none";
+  };
   return (
     <>
       <div className="list-courses-wrapper">
         <div className="top">
           <h3>Danh sách giáo viên</h3>
           <button onClick={handlePopupAddTeachForm}>
-            <i className="fa-solid fa-plus"></i>Thêm khóa học
+            <i className="fa-solid fa-plus"></i>Thêm giáo viên
           </button>
         </div>
         <Table
@@ -36,6 +46,92 @@ function ListTeacher() {
           data={data}
           handleUpdate={handlePopupUpdateTeacherForm}
         />
+        <div className="add-course" id="add-teacher">
+          <form action="" className="form-add">
+            <i
+              className="fa-solid fa-xmark close"
+              onClick={handleCloseAddTeacherForm}
+            ></i>
+            <h3>Thêm giáo viên</h3>
+            <div className="input-field">
+              <label htmlFor="">Tên giáo viên:</label>
+              <br />
+              <input type="text" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="">Email:</label>
+              <br />
+              <input type="email" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="">Ngày sinh:</label>
+              <br />
+              <input type="date" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="">Giới tính:</label>
+              <br />
+              <select>
+                <option selected disabled>
+                  --Giới tính--
+                </option>
+                <option>Nam</option>
+                <option>Nữ</option>
+              </select>
+            </div>
+
+            <button type="submit">Thêm</button>
+          </form>
+        </div>
+        <div className="update-course" id="update-teacher">
+          <form action="" className="form-add">
+            <i
+              className="fa-solid fa-xmark close"
+              onClick={handleCloseUpdateTeacherForm}
+            ></i>
+            <h3>Cập nhật giáo viên</h3>
+            <div className="input-field">
+              <label htmlFor="">Tên giáo viên:</label>
+              <br />
+              <input type="text" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="">Email:</label>
+              <br />
+              <input type="email" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="">Ngày sinh:</label>
+              <br />
+              <input type="date" />
+            </div>
+            <div className="input-field">
+              <label htmlFor="">Giới tính:</label>
+              <br />
+              <select>
+                <option selected disabled>
+                  --Giới tính--
+                </option>
+                <option>Nam</option>
+                <option>Nữ</option>
+              </select>
+            </div>
+            <div className="input-field">
+              <label htmlFor="">Trạng thái:</label>
+              <br />
+              <select>
+                <option selected disabled>
+                  --Trạng thái--
+                </option>
+                <option>Hoạt động</option>
+                <option>Ngưng hoạt động</option>
+              </select>
+            </div>
+            <button type="submit">
+              <i className="fa-solid fa-plus"></i> Thêm
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
