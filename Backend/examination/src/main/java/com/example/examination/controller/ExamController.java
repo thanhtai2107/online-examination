6467943a89f2bbcb2afc22d8619dd7ddf42070a3
thanhtai2107 <thanhtai21701@gmail.com>
2024-04-10 15:35:00 +0700

    feat: display list exams

--- Backend/examination/src/main/java/com/example/examination/controller/ExamController.java
+++ Backend/examination/src/main/java/com/example/examination/controller/ExamController.java
@@ -5,6 +5,7 @@ import com.example.examination.exception.CourseException;
 import com.example.examination.request.AddExamReq;
 import com.example.examination.service.impl.ExamServiceImpl;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,5 +23,11 @@ public class ExamController {
     @PostMapping("/exam/add")
     public ResponseEntity<ExamDTO> addExam(@RequestBody @Valid AddExamReq req) throws CourseException {
         return new ResponseEntity<>(examService.addExam(req), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/exams")
+    public ResponseEntity<Page<ExamDTO>> getExams(@RequestParam(defaultValue = "0") int page,
+                                                  @RequestParam(defaultValue = "5") int size) {
+        return ResponseEntity.ok(examService.getExams(page, size));
     }
 }
--- Backend/examination/src/main/java/com/example/examination/service/IExamService.java
+++ Backend/examination/src/main/java/com/example/examination/service/IExamService.java
@@ -4,7 +4,9 @@ import com.example.examination.dto.ExamDTO;
 import com.example.examination.exception.CourseException;
 import com.example.examination.exception.ExamException;
 import com.example.examination.request.AddExamReq;
+import org.springframework.data.domain.Page;
 
 public interface IExamService {
     ExamDTO addExam(AddExamReq req) throws  CourseException;
+    Page<ExamDTO> getExams(int page, int size);
 }
